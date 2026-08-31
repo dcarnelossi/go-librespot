029996a046093d7a9e26412c06518bc9012843ad
dcarnelossi <dcarnelossi@gmail.com>
2026-08-31 10:48:41 -0300

    feat: add player audio export helper

---
+++ player/audio_export.go
@@ -0,0 +1,32 @@
+package player
+
+import (
+	"encoding/hex"
+	"io"
+
+	librespot "github.com/devgianlu/go-librespot"
+	"github.com/devgianlu/go-librespot/audioexport"
+)
+
+var oggExporter *audioexport.Exporter
+
+func configureOggExporter(log librespot.Logger, enabled bool, directory string, overwrite bool) {
+	if !enabled {
+		oggExporter = nil
+		return
+	}
+	oggExporter = audioexport.New(log, directory, overwrite)
+}
+
+func (p *Player) exportOgg(fileID []byte, encrypted io.ReaderAt, size int64, key []byte) {
+	if oggExporter == nil {
+		return
+	}
+	file := hex.EncodeToString(fileID)
+	path, err := oggExporter.Export(fileID, encrypted, size, key)
+	if err != nil {
+		p.log.WithError(err).WithField("file", file).Warnf("audio export failed")
+		return
+	}
+	p.log.WithField("file", file).WithField("path", path).Infof("audio export completed")
+}
